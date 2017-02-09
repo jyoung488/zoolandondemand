61c9dd352bf24d248afd2630e15f9a0ee6a9681e
jyoung488 <jyoung488@gmail.com>
2017-02-08 23:27:33 -0800

    Add Zoolander-Rails gem and remove data

--- app/models/zoolander_quote.rb
+++
@@ -1,8 +0,0 @@
-class ZoolanderQuote < ActiveRecord::Base
-
-  def self.random
-    random_id = rand(ZoolanderQuote.first.id..ZoolanderQuote.last.id)
-    ZoolanderQuote.find(random_id).body
-  end
-
-end
--- db/migrate/20170108184444_create_zoolander_quotes.rb
+++
@@ -1,9 +0,0 @@
-class CreateZoolanderQuotes < ActiveRecord::Migration[5.0]
-  def change
-    create_table :zoolander_quotes do |t|
-      t.text :body
-
-      t.timestamps null: false
-    end
-  end
-end
